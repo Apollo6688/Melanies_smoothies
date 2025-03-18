0036e93a14fe881c1e805ac00cade424560d4d91
Apollo <82961524+Apollo6688@users.noreply.github.com>
2025-03-18 11:09:56 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 import requests    
-moothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+moothiefroot_response = requests.get("https://my.SMOOTHIEFROOT.com/api/fruit/watermelon")
 st.text(smoothiefroot_response) 
 
 helpful_links = [
