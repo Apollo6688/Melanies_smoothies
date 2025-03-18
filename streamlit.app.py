4b26cd8ea96807e344d07e7c6415434c5db53a92
Apollo <82961524+Apollo6688@users.noreply.github.com>
2025-03-18 11:21:28 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -3,10 +3,6 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-import requests    
-moothiefroot_response = requests.get("https://https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json()) 
-
 helpful_links = [
     "https://docs.streamlit.io",
     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
@@ -57,4 +53,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-   
+#New section to display Smoothiefroot nutrition
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
