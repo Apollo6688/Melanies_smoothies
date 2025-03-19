549b220fc115891a2ef0ed838e93da8eda2b542b
Apollo <82961524+Apollo6688@users.noreply.github.com>
2025-03-19 12:09:26 +0100

    Update streamlit.app.py
    Correct a typo

--- streamlit.app.py
+++ streamlit.app.py
@@ -16,7 +16,7 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 title = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be:",title)
 
-cnx = st.connection("snowlake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
