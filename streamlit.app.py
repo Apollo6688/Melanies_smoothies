6198abf535ead62f26e6791bd89fdd0953a0b895
Apollo <82961524+Apollo6688@users.noreply.github.com>
2025-03-19 11:23:07 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+from snowflake.snowpark import SnowparkSessionException
 import requests
 
 helpful_links = [
